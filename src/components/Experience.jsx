53ee6616d635c3d4fd7336af2b73158f44b5dc0c
Lucas Lernoud <lucaslerdb@gmail.com>
2023-10-16 20:42:00 +0200

    3d model + animtions

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,21 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        <Avatar animation={animation} />
+      </group>
+      <ambientLight intensity={1} />
     </>
   );
 };
